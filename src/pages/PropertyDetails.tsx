83cc95203631dfa6eb0f84dd0fdeb5b464b2345e
Eduarda-scs <mariaeduardacosta.exe@gmail.com>
2025-12-30 10:08:06 -0300

    link previw

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -21,7 +21,7 @@ import { useNavigate } from "react-router-dom";
 import { LeadModal } from "@/components/leadmodal";
 
 import { getUniquePropertyFromWebhook } from "@/hooks/Admin/ClientProperty";
-=======
+
 import {
   getPropertyFromWebhook,
   getUniquePropertyFromWebhook,
