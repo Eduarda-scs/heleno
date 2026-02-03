b07851db4680010417c8a3e85cb748ae4fba9945
Eduarda-scs <mariaeduardacosta.exe@gmail.com>
2026-02-03 14:22:05 -0300

    adimin ajustado

--- src/views/Admin/PropertySearch.tsx
+++ src/views/Admin/PropertySearch.tsx
@@ -71,7 +71,7 @@ const PropertySearch = ({ onSearchResults, onClearSearch }: PropertySearchProps)
     // Alterado: Agora redireciona para /detalhe-imovel/id-do-imovel
     setShowSuggestions(false);
     setSearchName("");
-    navigate(`/detalhe-imovel/${property.id}`);
+    navigate(`/admin/detalhe-imovel/${property.id}`);
   };
 
   const handleSearch = async () => {
